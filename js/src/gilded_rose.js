a9c6500fd87776f347407373af494ec5ba226d3c
Georgios Psarakis <g.psarakis3800@gmail.com>
2019-04-14 14:07:40 -0400

    added ===

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -21,7 +21,7 @@ function update_quality() {
     } else {
       if (items[i].quality < 50) {
         items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+        if (items[i].name === 'Backstage passes to a TAFKAL80ETC concert') {
           if (items[i].sell_in < 11) {
             if (items[i].quality < 50) {
               items[i].quality = items[i].quality + 1
