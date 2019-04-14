47187f6ada53be21de865b278d759c5047280c5f
Georgios Psarakis <g.psarakis3800@gmail.com>
2019-04-14 14:02:28 -0400

    added some unit tests and a conjered function

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -1,16 +1,20 @@
 function Item(name, sell_in, quality) {
-  this.name = name;
-  this.sell_in = sell_in;
-  this.quality = quality;
+  this.name = name
+  this.sell_in = sell_in
+  this.quality = quality
 }
 
 var items = []
 
 function update_quality() {
   for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+    if (
+      items[i].name != 'Aged Brie' &&
+      items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
+    ) {
       if (items[i].quality > 0) {
         if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          is_conjured(items[i])
           items[i].quality = items[i].quality - 1
         }
       }
@@ -31,9 +35,11 @@ function update_quality() {
         }
       }
     }
+
     if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
+      items[i].sell_in = items[i].sell_in - 1
     }
+
     if (items[i].sell_in < 0) {
       if (items[i].name != 'Aged Brie') {
         if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -52,4 +58,16 @@ function update_quality() {
       }
     }
   }
+}
+
+is_conjured = item => {
+  if (item.name.includes('Conjured')) {
+    if (item.quality > 0) {
+      item.quality = item.quality - 1
+    }
+    if (item.sell_in < 0) {
+      item.quality = item.quality - 1
+    }
+    return item
+  } else return item
 }
